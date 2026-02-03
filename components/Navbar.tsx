f69002cf055403831cae21d2ae4a39cfe8fb0b85
tummybutters <your_github_email@example.com>
2026-02-02 16:52:41 -0800

    Make logo barely smaller

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -82,7 +82,7 @@ export default function Navbar() {
                 src="/images/logo.png"
                 alt="Malohn Capital Group"
                 fill
-                className={`object-contain ${isHome ? 'scale-[1.7]' : ''}`}
+                className={`object-contain ${isHome ? 'scale-[1.5]' : ''}`}
               />
             </div>
           </Link>
