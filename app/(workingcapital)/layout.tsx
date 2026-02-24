aebf160ab1f22a4d456c19a1cf4bd9deb69a6f5f
tummybutters <your_github_email@example.com>
2026-02-24 09:42:21 -0800

    Update Meta Pixel ID to 1279587374031641 for workingcapital landing page

--- app/(workingcapital)/layout.tsx
+++ app/(workingcapital)/layout.tsx
@@ -76,7 +76,7 @@ export default function SecretLandingLayout({
             t.src=v;s=b.getElementsByTagName(e)[0];
             s.parentNode.insertBefore(t,s)}(window, document,'script',
             'https://connect.facebook.net/en_US/fbevents.js');
-            fbq('init', '1284052240208847');
+            fbq('init', '1279587374031641');
             fbq('track', 'PageView');
           `}
         </Script>
@@ -85,7 +85,7 @@ export default function SecretLandingLayout({
             height="1"
             width="1"
             style={{ display: 'none' }}
-            src="https://www.facebook.com/tr?id=1284052240208847&ev=PageView&noscript=1"
+            src="https://www.facebook.com/tr?id=1279587374031641&ev=PageView&noscript=1"
             alt=""
           />
         </noscript>
