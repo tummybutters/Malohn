cfb8523934b2716e50eba119ace5be9956bd6ea6
tummybutters <your_github_email@example.com>
2026-03-07 12:01:07 -0800

    Update layout metadata icons

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -28,10 +28,15 @@ export const metadata: Metadata = {
     template: `%s | ${siteConfig.name}`,
   },
   description: siteConfig.description,
+  manifest: '/site.webmanifest',
   icons: {
-    icon: '/favicon.png',
-    shortcut: '/favicon-16.png',
-    apple: '/apple-touch-icon.png',
+    icon: [
+      { url: '/favicon.ico' },
+      { url: '/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
+      { url: '/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
+    ],
+    shortcut: '/favicon.ico',
+    apple: [{ url: '/apple-touch-icon.png', sizes: '180x180', type: 'image/png' }],
   },
   alternates: {
     canonical: '/',
